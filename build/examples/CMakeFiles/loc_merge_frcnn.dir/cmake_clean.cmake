0fb18a588f83dea78c00d4f6efd298d44c4f2541
nguyendinh1987 <nguyendinh1987@gmail.com>
2018-11-09 13:52:37 +0800

    modify image -> img

---
+++ build/examples/CMakeFiles/loc_merge_frcnn.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/loc_merge_frcnn.dir/FRCNN/loc_merge_frcnn.cpp.o"
+  "FRCNN/loc_merge_frcnn.pdb"
+  "FRCNN/loc_merge_frcnn"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/loc_merge_frcnn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/examples/CMakeFiles/loc_test_frcnn.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/loc_test_frcnn.dir/FRCNN/loc_test_frcnn.cpp.o"
+  "FRCNN/loc_test_frcnn.pdb"
+  "FRCNN/loc_test_frcnn"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/loc_test_frcnn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
